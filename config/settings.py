01e921161e9bfaa7a0087bf3683df8e3cef90ce3
VeguitaScript <cristiandelavegacuevas@gmail.com>
2023-11-27 01:22:52 -0500

    add python version

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
-
+PYTHON_VERSION = 3.11
 # Application definition
 
 INSTALLED_APPS = [
